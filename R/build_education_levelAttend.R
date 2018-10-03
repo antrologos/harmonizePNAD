722a02cc118d279c78e29614a528e5108d9cc758
Rogerio Jeronimo Barbosa <antrologos@gmail.com>
2018-10-03 15:03:25 -0300

    new variables: "school attendance" and "level attended"

---
+++ R/build_education_levelAttend.R
@@ -0,0 +1,34 @@
+#' Builds a synthetic variable for education attainment
+#' @param data.frame
+#' @value data.frame
+#' @export
+
+build_education_levelAttend <- function(Data, ajust_for_age = T){
+
+        just_created_vars_list_existedBefore <- exists(x = "just_created_vars", where = .GlobalEnv)
+
+        Data <- harmonizePNAD:::check_prepared_to_harmonize(Data)
+
+        sulfix <- harmonizePNAD:::find_sulfix(Data, general_or_specific = "general")
+        call   <- paste0("harmonizePNAD:::build_education_levelAttend_", sulfix, "(Data)")
+        Data   <- eval(parse(text = call))
+        gc()
+
+        if(ajust_for_age == T){
+                Data <- harmonizePNAD:::check_and_build_onTheFly(Data,
+                                                                 var_name = "age",
+                                                                 general_or_specific = "general")
+
+                Data[age < 5 , levelAttend := NA]
+
+                if(just_created_vars_list_existedBefore == F){
+                        Data <- harmonizePNAD:::erase_just_created_vars(Data)
+                }
+        }
+
+        gc(); Sys.sleep(.3); gc()
+
+        Data
+
+}
+
---
+++ R/build_education_levelAttend_pnad.R
@@ -0,0 +1,64 @@
+build_education_levelAttend_pnad <- function(Data){
+
+        # Loading the crosswalk
+        file_location <- system.file("extdata",
+                                     "crosswalk_pnad_levelAttend.csv",
+                                     package = "harmonizePNAD")
+        file_location = "C:/Users/Rogerio/Google Drive/RCodes/PacotesR/harmonizePNAD/inst/extdata/crosswalk_pnad_levelAttend.csv"
+        crosswalk   <- data.table::fread(file_location)
+
+        # Selecting the appropriate crosswalk for the current year
+        metadata    <- harmonizePNAD:::get_metadata(Data)
+
+        selected_lines <- NULL
+        for(i in 1:nrow(crosswalk)){
+                test <- crosswalk[i , metadata$year %in% eval(parse(text = year))]
+
+                if(test == TRUE){
+                        selected_lines <- c(selected_lines, i)
+                }
+        }
+        crosswalk_i <- crosswalk[selected_lines]
+
+        # Checking the variable availability
+        necessary_vars <- dplyr::select(crosswalk_i, starts_with("var")) %>%
+                as.matrix() %>%
+                as.character() %>%
+                unique()
+
+        necessary_vars <- necessary_vars[nchar(necessary_vars) > 1]
+
+        harmonizePNAD:::check_necessary_vars(Data, necessary_vars)
+
+        # Recoding
+        Data[ , levelAttend := as.numeric(NA)]
+
+        for(j in 1:nrow(crosswalk_i)){
+                var_grau      = crosswalk_i[j,]$var_grau
+                var_serie     = crosswalk_i[j,]$var_serie
+                var_condition = crosswalk_i[j,]$var_condition
+
+                value_grau      = crosswalk_i[j,]$value_grau
+                value_serie     = crosswalk_i[j,]$value_serie
+                value_condition = crosswalk_i[j,]$value_condition
+
+                finalvalue = crosswalk_i[j,]$finalvalue %>% as.numeric()
+
+                exp_condition = paste(var_grau,"%in% c(",value_grau,")")
+
+                if(nchar(var_serie) > 1){
+                        exp_condition <- paste(exp_condition, "&", var_serie,"%in% c(",value_serie,")")
+                }
+
+                if(nchar(var_condition) > 1){
+                        exp_condition <- paste(exp_condition, "&", var_condition,"%in% c(",value_condition,")")
+                }
+
+                Data[eval(parse(text = exp_condition)),    levelAttend := finalvalue]
+        }
+        gc()
+
+        Data
+}
+
+
